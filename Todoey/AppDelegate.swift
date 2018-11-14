5283cd23d6c46dc264f6a5b1d5e741f4fd249284
R3novatio <kingr3novatio@gmail.com>
2018-11-13 20:45:27 -0500

    Persisting Data Using Realm

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-import CoreData
+
 import RealmSwift
 
 @UIApplicationMain
@@ -17,11 +17,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+//        print(Realm.Configuration.defaultConfiguration.fileURL)
       
     
         
         do {
-            let realm = try Realm()
+             _ = try Realm()
             } catch {
             print("error initialising new realm, \(error)")
         }
@@ -30,39 +32,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
     }
         
-    func applicationWillTerminate(_ application: UIApplication) {
-        
-        self.saveContext()
+ 
     }
     
-    //MARK: Core Data Stack
     
-    lazy var persistentContainer: NSPersistentContainer = {
-        
-        let container = NSPersistentContainer(name: "DataModel")
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-            
-           
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        return container
-    }()
-    
-    // Mark: - Core Data Saving Support
-    
-    func saveContext () {
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-            
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
-        }
-    }
-    
-}
+
